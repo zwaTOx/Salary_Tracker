444066fa66c7a230492318a57ec3261661c0dc47
Yakov Grechnev <yakov.g.ruslanovich@gmail.com>
2025-06-21 18:22:01 +0700

    add user model

---
+++ src/models/user_model.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timezone
+from sqlalchemy import (
+    Column,
+    DateTime, Integer, String 
+    )
+from src.database import Base
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), default='user')
+    email = Column(String(100), unique=True)
+    hashed_password = Column(String(256))
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
