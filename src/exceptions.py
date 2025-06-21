0552aa03c7a19f19c67aef8dce59602f5d6fad2e
Yakov Grechnev <yakov.g.ruslanovich@gmail.com>
2025-06-21 22:01:22 +0700

    add custom exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,16 @@
+from fastapi import HTTPException, status
+
+class BaseAPIException(HTTPException):
+    status_code: int
+
+    def __init__(self, detail: str = None):
+        super().__init__(
+            status_code=self.status_code,
+            detail=detail
+        )
+
+class NotFoundException(BaseAPIException):
+    status_code = status.HTTP_404_NOT_FOUND
+
+class BadRequestException(BaseAPIException):
+    status_code = status.HTTP_400_BAD_REQUEST
